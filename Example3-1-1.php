28eafc09f20edc7400dd3d1312062e47d65b9aef
MFJLBeuzel <maartenbeuzel@gmail.com>
2016-07-05 01:56:58 +0200

    Commit
    4edag

--- Example3-1-1.php
+++ Example3-1-1.php
@@ -1,3 +1,23 @@
 //overweeg het volgende om de verwarring met gebruik = en == te ontwijken
 
 <?php
+if (12 == $new_messages) {
+    print "BLABLABLA.";
+}
+ ?>
+
+<?php
+if ($new_messages != 10) {
+    print "Dit is de not-equal operator.";
+}
+ ?>
+
+<?php
+if ($celsius_temp <= 0) {
+    print "ow, wat koud!!";
+}
+ ?>
+
+<?php
+
+ ?>
