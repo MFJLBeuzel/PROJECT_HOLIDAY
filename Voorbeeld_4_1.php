bc6194ef7a7a3c655c266fcbaa91efc1a17a4376
MFJLBeuzel <maartenbeuzel@gmail.com>
2016-07-19 01:32:11 +0200

    Commit
    commit

---
+++ Voorbeeld_4_1.php
@@ -0,0 +1,20 @@
+<?php
+    echo "a: [" . (20 > 9) . "]<br />";
+    echo "b: [" . (5 == 6) . "]<br />";
+    echo "c: [" . (1 == 0) . "]<br />";
+    echo "d: [" . (1 == 1) . "]<br />";
+ ?>
+<br />
+<?php
+    echo "a: [" . TRUE . "]<br />";
+    echo "b: [" . FALSE . "]<br />";
+ ?>
+<br />
+<?php
+    echo "a: [" . 73 . "]<br />";
+    echo "b: [" . "Hello" . "]<br />";
+    echo "c: [" . FALSE . "]<br />";
+    echo "d: [" . $myname . "]<br />";
+    echo "e: [" . $myage . "]<br />";
+ ?>
+ 
